36b4e4e67e6b2d45ae7e9138c0e4b45fa2497e2f
Alrick Deillon <alrick.deillon@gmail.com>
2012-08-23 10:40:31 +0200

    Generated folders bug fix
    Avoid special chars, spaces and duplicate

--- redmine_reddrop/app/controllers/generatedfolders_controller.rb
+++ redmine_reddrop/app/controllers/generatedfolders_controller.rb
@@ -12,25 +12,41 @@ class GeneratedfoldersController < ApplicationController
   end
 
   def create
-    g = Generatedfolder.new(:name => params[:name])
-    if g.save
-      flash[:notice] = "Folder successfully created."
-      redirect_to :action => "index"
+    name = params[:name] # get name
+    name = check_name(name) # correct name
+
+    if !check_exists(name)
+      g = Generatedfolder.new(:name => name)
+      if g.save
+        flash[:notice] = "Folder successfully created."
+        redirect_to :action => "index"
+      else
+        flash[:error] = "Error while creating folder, please retry."
+        redirect_to :action => "index"
+      end
     else
-      flash[:error] = "Error while creating folder, please retry."
+      flash[:error] = "This folder already exists."
       redirect_to :action => "index"
     end
   end
 
   def update
     g = Generatedfolder.find(params[:id])
-    g.name = params[:name]
-    if g.save
-      flash[:notice] = "Folder successfully updated."
-      redirect_to :action => "index"
+    name = params[:name] # get name
+    name = check_name(name) # correct name
+
+    if !check_exists(name)
+      g.name = name
+      if g.save
+        flash[:notice] = "Folder successfully updated."
+        redirect_to :action => "index"
+      else
+        flash[:error] = "Error while updating folder, please retry."
+        redirect_to :action => "edit"
+      end
     else
-      flash[:error] = "Error while updating folder, please retry."
-      redirect_to :action => "index"
+      flash[:error] = "This folder already exists."
+      redirect_to :action => "edit"
     end
   end
 
@@ -50,5 +66,14 @@ class GeneratedfoldersController < ApplicationController
     else
       redirect_to :controller => "welcome", :action => "index"
     end
+  end
+
+  def check_name(name)
+    name = name.tr(' ', '_') # replace spaces
+    name = name.gsub(/[^0-9A-Za-z_]/, '') # replace special chars but _
+  end
+
+  def check_exists(name)
+    Generatedfolder.exists?(:name => name)
   end
 end
